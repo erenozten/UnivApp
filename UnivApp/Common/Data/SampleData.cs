a30fc07e6290243e0a717a9c879f97e6646d4567
ErenOzten <priimalshade@gmail.com>
2020-09-30 03:25:02 +0300

    reset

---
+++ UnivApp/Common/Data/SampleData.cs
@@ -0,0 +1,18 @@
+﻿using ContosoUniversity.Data.DTO;
+using UnivApp.Common.DTO;
+using System.Collections.Generic;
+
+namespace UnivApp.Common.Data
+{
+    public class SampleData
+    {
+        public List<EnrollmentDTO> Enrollments { get; set; }
+        public List<CourseDTO> Courses { get; set; }
+        public List<CourseAssignmentsDTO> CourseAssignments { get; set; }
+        public List<DepartmentDTO> Departments { get; set; }
+        public List<StudentDTO> Students { get; set; }
+        public List<OfficeAssignmentDTO> OfficeAssignments { get; set; }
+        public List<InstructorDTO> Instructors { get; set; }
+        public bool SaveToExternalFile { get; set; }
+    }
+}
